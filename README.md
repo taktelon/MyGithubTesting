010a1126a8277fd3782587e9fcf22b1a0fe2b82e
taktelon <taktelon@gmail.com>
2020-06-08 09:48:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is Github configure testing
+## Learn how to
+- Create project in local
+- upload them to github
---
+++ src/main/java/com/taktelon/mytest/Main.java
@@ -0,0 +1,7 @@
+package com.taktelon.mytest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+    }
+}
